b3fe04be2255911075d15d31350e7c4d966d401a
Dennis Boateng <dboateng@acsint.io>
2022-10-09 23:21:18 +0000

    added express and create react app template to project

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    
+<div class="grid grid-cols-2 gap-4">
+  <div>01</div>
+  <div>09</div>
+</div>
+
+  )
+}
+
+export default Navbar
